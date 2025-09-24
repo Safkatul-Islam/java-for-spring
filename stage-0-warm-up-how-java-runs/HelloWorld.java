78107fedf14c248661ff216c7e60025660ca13d5
Md Safkatul Islam <mrsafkatul.stu24@gmail.com>
2025-09-24 15:16:09 -0700

    Done with stage 0

---
+++ stage-0-warm-up-how-java-runs/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Shafkat\nI'm learning Java\nSpring Boot, here I come!");
+    }
+}
